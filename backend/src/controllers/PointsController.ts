31d70d0382f6d40f96e5aa5630b04e292dee7cb3
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-10-11 15:01:41 -0300

    img padrao

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -32,7 +32,7 @@ class PointsController {
     const trx = await knex.transaction();
     
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
       name, 
       email,
       whatsapp,
